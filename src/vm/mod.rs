6ce723f3cf9be25ffc9159c4a5e849da1c51e814
luki <letitbegos@gmail.com>
2023-09-27 23:49:11 +0800

    vm stack optimization

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -9,7 +9,7 @@ use crate::{
     vm::object::StringObject,
 };
 use rustc_hash::FxHasher;
-
+use std::{mem, ptr};
 pub mod chunk;
 pub mod compiler;
 pub mod object;
@@ -20,7 +20,8 @@ pub mod value;
 pub struct VM<'a> {
     chunk: chunk::Chunk,
     ip: usize,
-    stack: Vec<value::Value>,
+    stack: Box<[Value; 256]>,
+    stack_top: *mut Value,
     allocator: &'a mut CeAllocation,
     globals: HashMap<*mut StringObject, Value, BuildHasherDefault<FxHasher>>,
 }
@@ -30,13 +31,15 @@ impl<'a> VM<'a> {
         VM {
             chunk,
             ip: 0,
-            stack: Vec::new(),
+            stack: Box::new([Value::Number(0.0); 256]),
+            stack_top: ptr::null_mut(),
             allocator,
             globals: HashMap::with_hasher(BuildHasherDefault::<FxHasher>::default()),
         }
     }
 
     pub fn run(&mut self) {
+        self.stack_top = self.stack.as_mut_ptr();
         loop {
             self.chunk.disassemble_instruction(self.ip);
             match self.read_byte() {
@@ -44,7 +47,7 @@ impl<'a> VM<'a> {
                 op::SET_LOCAL => self.set_local(),
                 op::SET_GLOBAL => self.set_global(),
                 op::PRINT => {
-                    let value: value::Value = self.stack.pop().unwrap();
+                    let value: value::Value = self.pop();
                     println!("{}", value);
                 }
                 op::ADD => self.add(),
@@ -56,7 +59,7 @@ impl<'a> VM<'a> {
                 op::NEG => self.negate(),
                 op::CECILE_CONSTANT => {
                     let constant = self.read_constant();
-                    self.stack.push(constant);
+                    self.push(constant);
                 }
                 op::DEFINE_GLOBAL => self.define_global(),
                 op::GET_GLOBAL => self.get_global(),
@@ -64,40 +67,40 @@ impl<'a> VM<'a> {
                 op::FALSE => self.op_false(),
                 op::NIL => self.op_nil(),
                 op::POP => {
-                    self.stack.pop();
+                    self.pop();
                 }
                 op::RETURN => {
-                    self.stack.pop();
+                    self.pop();
                     return;
                 }
                 _ => todo!(),
             }
             //print top of stack element
-            for (i, value) in self.stack.iter().enumerate() {
-                print!("[ {:?} ]", value);
+            let mut stack_ptr = self.stack.as_mut_ptr();
+            while stack_ptr < self.stack_top {
+                print!("[ {} ]", unsafe { *stack_ptr });
+                stack_ptr = unsafe { stack_ptr.add(1) };
             }
             println!();
         }
     }
 
     fn get_local(&mut self) {
-        let index = self.read_byte() as usize;
-        let stack_idx = self.chunk.constants[index].clone();
-        match stack_idx {
-            Value::Number(stack_idx) => {
-                let value = self.stack[stack_idx as usize];
-                self.stack.push(value);
+        let stack_index = self.read_constant();
+        match stack_index {
+            Value::Number(stack_index) => {
+                let value = self.stack[stack_index as usize];
+                self.push(value);
             }
             _ => todo!(),
         }
     }
 
     fn set_local(&mut self) {
-        let constant_index = self.read_byte() as usize;
-        let stack_index = self.chunk.constants[constant_index].clone();
+        let stack_index = self.read_constant();
         match stack_index {
             Value::Number(stack_index) => {
-                let value = self.stack.pop().unwrap();
+                let value = self.pop();
                 self.stack[stack_index as usize] = value;
             }
             _ => todo!(),
@@ -105,7 +108,7 @@ impl<'a> VM<'a> {
     }
 
     fn set_global(&mut self) {
-        let value = self.stack.pop().unwrap();
+        let value = self.pop();
         let name = self.read_constant();
         match name {
             Value::String(ptr_string) => match self.globals.entry(ptr_string) {
@@ -125,7 +128,7 @@ impl<'a> VM<'a> {
         match name {
             Value::String(ptr_string) => match self.globals.get(&ptr_string) {
                 Some(value) => {
-                    self.stack.push(*value);
+                    self.push(*value);
                 }
                 None => {
                     todo!()
@@ -136,7 +139,7 @@ impl<'a> VM<'a> {
     }
 
     fn define_global(&mut self) {
-        let value = self.stack.pop().unwrap();
+        let value = self.pop();
         let name = self.read_constant();
         let name = match name {
             Value::String(ptr_string) => {
@@ -147,15 +150,33 @@ impl<'a> VM<'a> {
     }
 
     fn op_nil(&mut self) {
-        self.stack.push(value::Value::Number(0.0));
+        self.push(value::Value::Number(0.0));
     }
 
     fn op_true(&mut self) {
-        self.stack.push(true.into());
+        self.push(true.into());
     }
 
     fn op_false(&mut self) {
-        self.stack.push(false.into());
+        self.push(false.into());
+    }
+
+    fn push(&mut self, value: Value) {
+        unsafe {
+            ptr::write(self.stack_top, value);
+            self.stack_top = self.stack_top.add(1);
+        }
+    }
+
+    fn pop(&mut self) -> Value {
+        unsafe {
+            self.stack_top = self.stack_top.sub(1);
+            ptr::read(self.stack_top)
+        }
+    }
+
+    fn peek(&self, distance: usize) -> *mut Value {
+        unsafe { self.stack_top.sub(distance + 1) }
     }
 
     fn read_constant(&mut self) -> value::Value {
@@ -171,12 +192,12 @@ impl<'a> VM<'a> {
     }
 
     fn add(&mut self) {
-        let rhs = self.stack.pop().unwrap();
-        let lhs = self.stack.pop().unwrap();
+        let rhs = self.pop();
+        let lhs = self.pop();
         match (lhs, rhs) {
             (value::Value::Number(lhs), value::Value::Number(rhs)) => {
                 // println!(" adding {} + {}", lhs, rhs);
-                self.stack.push(Value::Number(lhs + rhs));
+                self.push(Value::Number(lhs + rhs));
             }
             (value::Value::String(lhs), value::Value::String(rhs)) => {
                 let mut lhs = unsafe { (*lhs).value.clone() };
@@ -186,47 +207,47 @@ impl<'a> VM<'a> {
                 // let string_obj = StringObject::new(static_str);
                 let ptr_string = self.alloc(string);
                 let string = Value::String(ptr_string);
-                self.stack.push(string);
+                self.push(string);
             }
             _ => todo!(),
         }
     }
 
     fn sub(&mut self) {
-        let rhs = self.stack.pop().unwrap();
-        let lhs = self.stack.pop().unwrap();
-        self.stack.push(lhs.sub(rhs));
+        let rhs = self.pop();
+        let lhs = self.pop();
+        self.push(lhs.sub(rhs));
     }
 
     fn mul(&mut self) {
-        let rhs = self.stack.pop().unwrap();
-        let lhs = self.stack.pop().unwrap();
-        self.stack.push(lhs.mul(rhs));
+        let rhs = self.pop();
+        let lhs = self.pop();
+        self.push(lhs.mul(rhs));
     }
 
     fn div(&mut self) {
-        let rhs = self.stack.pop().unwrap();
-        let lhs = self.stack.pop().unwrap();
-        self.stack.push(lhs.div(rhs));
+        let rhs = self.pop();
+        let lhs = self.pop();
+        self.push(lhs.div(rhs));
     }
 
     fn equal(&mut self) {
-        let rhs = self.stack.pop().unwrap();
-        let lhs = self.stack.pop().unwrap();
-        self.stack.push((lhs == rhs).into());
+        let rhs = self.pop();
+        let lhs = self.pop();
+        self.push((lhs == rhs).into());
     }
 
     fn not_equal(&mut self) {
-        let rhs = self.stack.pop().unwrap();
-        let lhs = self.stack.pop().unwrap();
+        let rhs = self.pop();
+        let lhs = self.pop();
 
-        self.stack.push((lhs != rhs).into());
+        self.push((lhs != rhs).into());
     }
 
     fn negate(&mut self) {
-        let value: value::Value = self.stack.pop().unwrap();
+        let value: value::Value = self.pop();
         // Value to f64
-        self.stack.push(value.neg());
+        self.push(value.neg());
     }
 
     fn alloc<T>(&mut self, object: impl CeAlloc<T>) -> T {
