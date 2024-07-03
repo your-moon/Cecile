1a20b7050ac863572c4aa35ce1b03c198a06a9be
Munkherdene Erdenebat <munkherdeneerdenebat@Munkherdenes-MacBook-Pro.local>
2024-07-04 00:08:10 +0800

    playground update

--- playground/cecile_playground/src/app/playground/page.tsx
+++ playground/cecile_playground/src/app/playground/page.tsx
@@ -1,9 +1,5 @@
 import Playground from "../components/playground";
 
 export default async function PlaygroundPage() {
-  return (
-    <div className="bg-sky-300 h-screen w-screen">
-      <Playground></Playground>
-    </div>
-  );
+  return <Playground></Playground>;
 }
